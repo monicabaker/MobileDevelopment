f5da1968ef8d63f77069dd18cc69edb9c974d5be
MonicaBaker <monicabakerapple@gmail.com>
2016-09-25 11:40:46 -0500

    New Version of MBaker Assignment1 Solved the problem the vary for traits is slightly better at least switch from vertical to horizontal views

---
+++ Assignments/MBakerAssignment1Final/MBakerAssignment1Final/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MBakerAssignment1Final
+//
+//  Created by Monica Baker on 9/25/16.
+//  Copyright © 2016 Monica Baker. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Assignments/MBakerAssignment1Final/MBakerAssignment1Final/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MBakerAssignment1Final
+//
+//  Created by Monica Baker on 9/25/16.
+//  Copyright © 2016 Monica Baker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
