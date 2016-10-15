36bd71b30c48d659352d8e9bfb9568d89e0f2867
MonicaBaker <monicabakerapple@gmail.com>
2016-10-15 13:28:34 -0500

    More changes

--- Assignments/MBakerAssignment2/MBakerAssignment2/Problem2ViewController.swift
+++ Assignments/MBakerAssignment2/MBakerAssignment2/Problem2ViewController.swift
@@ -9,23 +9,34 @@
 import UIKit
 
 class Problem2ViewController: UIViewController {
- 
+    
     @IBOutlet weak var textField: UITextField!
-        
-   override func viewDidLoad() {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Problem 2"
-        // Do any additional setup after loading the view, typically from a nib.
-        }
-        
+        // Do any additional setup after loading the view.
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        }
-        
+    }
+    
     @IBAction func performCalculation(_ sender: AnyObject) {
         print("Hey the button got pushed")
         textField.text = Date().description
-        }
-
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
