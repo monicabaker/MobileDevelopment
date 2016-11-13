91c21581428a946a2187e9dbc5be773fd0f91e0f
MonicaBaker <monicabakerapple@gmail.com>
2016-11-13 13:38:22 -0600

    MB Final Project more revisions 11.13.16

---
+++ Project/MBFinalProject/MBFinalProject/InstrumentationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InstrumentationViewController.swift
+//  MBFinalProject
+//
+//  Created by Monica Baker on 11/13/16.
+//  Copyright © 2016 Monica Baker. All rights reserved.
+//
+
+import UIKit
+
+class InstrumentationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Project/MBFinalProject/MBFinalProject/SimulationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SimulationViewController.swift
+//  MBFinalProject
+//
+//  Created by Monica Baker on 11/13/16.
+//  Copyright © 2016 Monica Baker. All rights reserved.
+//
+
+import UIKit
+
+class SimulationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
